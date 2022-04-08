687ff485471ff97492218feadd4fad3a5497d1b7
emaduddin678 <emaduddin678@gmail.com>
2022-04-08 15:00:50 +0600

    complete

--- problem-2.js
+++ problem-2.js
@@ -1,3 +1,6 @@
 const animals = ['rifat', 'shahin', 'rakib', 'redwan', 'mukta'];
 
-console.log(animals.splice(2,2))
+// console.log(animals.splice(2,2))
+animals.splice(0,2)
+animals.splice(2,1)
+console.log(animals)
